86702b3b430a2f3fcfff668f1debf654b669d49c
Yoel Benitez Fonseca <ybenitezf@gmail.com>
2025-04-14 13:33:15 -0400

    update README.md
    Also update lock files

--- update-locks.sh
+++ update-locks.sh
@@ -5,7 +5,7 @@ echo "Updating dependencies in projects"
 for dir in projects/*; do (
     cd "$dir"
     echo "Updating $dir"
-    poetry lock --no-update
+    poetry lock
 ); done
 # update dependencies in development
-poetry lock --no-update
+poetry lock
