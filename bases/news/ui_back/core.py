6a638a424570fd24e4e7cd4796aba076d78e47d0
Yoel Benitez Fonseca <ybenitezf@gmail.com>
2025-04-18 12:10:08 -0400

    Add appdirs for user data directory management

--- bases/news/ui_back/core.py
+++ bases/news/ui_back/core.py
@@ -1,6 +1,21 @@
+import logging
+from pathlib import Path
+
+from appdirs import AppDirs
 from fasthtml.common import Div, P, fast_app
 
-app, rt = fast_app()
+log = logging.getLogger("NewsEd")
+
+log.info("Creating application dirs")
+app_dirs = AppDirs("NewsEd", "adelante")
+app_data_dir = Path(app_dirs.user_data_dir)
+app_data_dir.mkdir(parents=True, exist_ok=True)
+uploads_dir = app_data_dir / "uploads"
+uploads_dir.mkdir(parents=True, exist_ok=True)
+log.info(f"User data dir is: ${app_data_dir}")
+
+
+app, rt = fast_app(key_fname=str(app_data_dir / ".sessionkey"), pico=True)
 
 
 @rt("/")
