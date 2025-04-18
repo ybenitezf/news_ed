9d17798959aa8d683386c00204dda9cc3b19d6ef
Yoel Benitez Fonseca <ybenitezf@gmail.com>
2025-04-18 12:23:46 -0400

    fix: add backend executable os independent

--- projects/news_ed_app/forge.config.ts
+++ projects/news_ed_app/forge.config.ts
@@ -7,14 +7,19 @@ import { AutoUnpackNativesPlugin } from "@electron-forge/plugin-auto-unpack-nati
 import { WebpackPlugin } from "@electron-forge/plugin-webpack";
 import { FusesPlugin } from "@electron-forge/plugin-fuses";
 import { FuseV1Options, FuseVersion } from "@electron/fuses";
-
+import * as path from "path";
 import { mainConfig } from "./webpack.main.config";
 import { rendererConfig } from "./webpack.renderer.config";
+
+let backend_path = path.join(".", "papp", "runner");
+if (process.platform === "win32") {
+    backend_path = path.join(".", "papp", "runner.exe");
+}
 
 const config: ForgeConfig = {
     packagerConfig: {
         asar: true,
-        extraResource: ["./papp/runner"],
+        extraResource: [backend_path],
     },
     rebuildConfig: {},
     makers: [
